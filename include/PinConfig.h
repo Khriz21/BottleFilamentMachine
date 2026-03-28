a4b5a9e3743d5bd621594024f888ac589eb05255
Khriz21 <89041458+Khriz21@users.noreply.github.com>
2026-03-28 14:00:30 +0100

    Update PinConfig.h

--- include/PinConfig.h
+++ include/PinConfig.h
@@ -8,4 +8,15 @@ const uint8_t EN_CLK = 4;
 const uint8_t EN_DT = 2;
 const uint8_t EN_SW = 3;
 
+//? <=========EXTRUDER==========>
+
+const uint8_t H0_PIN = 5;
+const uint8_t H0_PIN = A0;
+const uint8_t EN_PIN = 8;
+const uint8_t DIR_PIN = 9;
+const uint8_t STP_PIN = 10;
+
+
+
+
 #endif
